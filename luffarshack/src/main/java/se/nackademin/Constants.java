b093494a854b62837cc4a5ff34641ddfb5a5efa0
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-24 08:47:17 +0100

    Added constants

---
+++ luffarshack/src/main/java/se/nackademin/Constants.java
@@ -0,0 +1,28 @@
+package se.nackademin;
+
+/**
+ * Class to handle constants
+ */
+
+public class Constants {
+
+    public static final String MOVE_MENU_TEXT = "########### MOVE ###########\n"
+            + "[w] - UP\n"
+            + "[s] - DOWN\n"
+            + "[a] - LEFT\n"
+            + "[d] - RIGHT\n"
+            + "[e] - To place ur marker\n"
+            + "[q] - Quit game (No save)\n"
+            + "###########################\n";
+
+    public static final String MAIN_MENU_TEXT = "########### MAIN MENU ###########\n"
+            + "[1] - Start A Two Player Game\n"
+            + "[2] - Start A Game VS The Computer\n"
+            + "[3] - View Stats\n"
+            + "[4] - Load A Saved Game\n"
+            + "[5] - View A Replay.\n"
+            + "[q] - Quit game\n"
+            + "#################################\n";
+
+    public static final String CHOOSE_NAME_TEXT = "######## ENTER YOUR NAME ########";
+}
