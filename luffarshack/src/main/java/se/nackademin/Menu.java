ccfc36f68a8d1e94c9ac82d3678808e5765a472f
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-24 08:48:30 +0100

    Added Menu interface

---
+++ luffarshack/src/main/java/se/nackademin/Menu.java
@@ -0,0 +1,22 @@
+// package se.nackademin;
+
+// /**
+//  * The interface for a menu.
+//  * It has two methods, checkValidInput and runMenu
+//  */
+// public interface Menu {
+//     /**
+//      * Checks if the input is valid.
+//      * @param choice The input from the user.
+//      * @return true if the input is valid, false otherwise.
+//      */
+//     public boolean handleChoice(String choice);
+
+//     /**
+//      * Runs the menu.
+//      * @param board  The current board.
+//      * @param player The current player.
+//      * @param game   The game object.
+//      */
+//     public void runMenu();
+// }
