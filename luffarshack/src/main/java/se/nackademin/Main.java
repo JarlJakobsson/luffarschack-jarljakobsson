28f709f2a40cf1e5ccf603102adf4cd500cfd7c2
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-24 08:47:56 +0100

    Added Main

---
+++ luffarshack/src/main/java/se/nackademin/Main.java
@@ -0,0 +1,14 @@
+package se.nackademin;
+
+/**
+ * Main class to start program.
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.runMenu();
+        }  
+}
+
