46b166e2aace82e423a2bd1681c9c437fa8ce09d
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-26 21:01:05 +0100

    Edited menu interface

--- luffarshack/src/main/java/se/nackademin/Menu.java
+++ luffarshack/src/main/java/se/nackademin/Menu.java
@@ -1,22 +1,31 @@
-// package se.nackademin;
+package se.nackademin;
 
-// /**
-//  * The interface for a menu.
-//  * It has two methods, checkValidInput and runMenu
-//  */
-// public interface Menu {
-//     /**
-//      * Checks if the input is valid.
-//      * @param choice The input from the user.
-//      * @return true if the input is valid, false otherwise.
-//      */
-//     public boolean handleChoice(String choice);
+/**
+ * The interface for a menu.
+ * It has two methods, checkValidInput and runMenu
+ * 
+ * Keeping some design thoughts, even though they couldnt be used after some refactoring
+ */
+public interface Menu {
+    // /**
+    //  * Checks if the input is valid.
+    //  * @param choice The input from the user.
+    //  * @return true if the input is valid, false otherwise.
+    //  */
+    // public boolean handleChoice(String choice);
 
-//     /**
-//      * Runs the menu.
-//      * @param board  The current board.
-//      * @param player The current player.
-//      * @param game   The game object.
-//      */
-//     public void runMenu();
-// }
+    /**
+     * Method to take input from user
+     * @param text takes in text print to the user what the expected input should be
+     * @return returns the users string
+     */
+    public String takeInput(String text);
+
+    // /**
+    //  * Runs the menu.
+    //  * @param board  The current board.
+    //  * @param player The current player.
+    //  * @param game   The game object.
+    //  */
+    // public void runMenu();
+}
