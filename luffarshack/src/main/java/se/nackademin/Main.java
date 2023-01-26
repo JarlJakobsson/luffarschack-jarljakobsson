96ab1697f5920a8498774d17df085c28532a83ad
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-26 02:05:13 +0100

    Added stats to main

--- luffarshack/src/main/java/se/nackademin/Main.java
+++ luffarshack/src/main/java/se/nackademin/Main.java
@@ -2,13 +2,12 @@ package se.nackademin;
 
 /**
  * Main class to start program.
+ * Creates a Stats object and a Mainmenu object and runs mainmenu
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        MainMenu mainMenu = new MainMenu();
+public class Main {
+    public static void main(String[] args) {
+        Stats stats = new Stats();
+        MainMenu mainMenu = new MainMenu(stats);
         mainMenu.runMenu();
-        }  
+    }
 }
-
