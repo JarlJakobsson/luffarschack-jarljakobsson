4dde3b040bea14a460bec5e948e7b3a653cc9de1
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-26 20:53:07 +0100

    Added storing moves, and improved AI

--- luffarshack/src/main/java/se/nackademin/Computer.java
+++ luffarshack/src/main/java/se/nackademin/Computer.java
@@ -1,12 +1,14 @@
 package se.nackademin;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Random;
 
 public class Computer implements Serializable {
     private InfiniteBoard board;
     private String mark = "0";
     private String opponentMark = "X";
+    private List<Move> moves;
 
     public Computer(InfiniteBoard board) {
         this.board = board;
@@ -20,6 +22,10 @@ public class Computer implements Serializable {
         this.mark = mark;
     }
 
+    public void setMoves(List<Move> moves) {
+        this.moves = moves;
+    }
+
     /**
      * Method to see if computer can win
      * Tries to place its more on each square one by one
@@ -28,12 +34,13 @@ public class Computer implements Serializable {
      * 
      * @return true if successfull winning move, false if not
      */
-    public boolean checkWin() {
+    public boolean checkWin(int numberForWin) {
         for (int i = 0; i < board.getSize(); i++) {
             for (int j = 0; j < board.getSize(); j++) {
                 if (board.getMark(i, j).equals(" ")) {
                     board.setMark(i, j, mark);
-                    if (board.checkWin(mark)) {
+                    if (board.checkWin(mark, numberForWin)) {
+                        moves.add(new Move(i, j, mark));
                         return true;
                     }
                     board.setMark(i, j, " ");
@@ -51,13 +58,14 @@ public class Computer implements Serializable {
      * 
      * @return true if successfully blocked the oppoenent, false if not
      */
-    public boolean checkBlock() {
+    public boolean checkBlock(int numberForWin) {
         for (int i = 0; i < board.getSize(); i++) {
             for (int j = 0; j < board.getSize(); j++) {
                 if (board.getMark(i, j).equals(" ")) {
                     board.setMark(i, j, opponentMark);
-                    if (board.checkWin(opponentMark)) {
+                    if (board.checkWin(opponentMark, numberForWin)) {
                         board.setMark(i, j, mark);
+                        moves.add(new Move(i, j, mark));
                         return true;
                     }
                     board.setMark(i, j, " ");
@@ -72,6 +80,8 @@ public class Computer implements Serializable {
      * Randomize a x and y cordinate within the board
      * If the square is markable, marks
      * if not continues the loop
+     * 
+     * @return true if successfull for testing purposes
      */
     public boolean randomMove() {
         while (true) {
@@ -80,6 +90,7 @@ public class Computer implements Serializable {
             int y = random.nextInt(board.getSize());
             if (board.getMark(y, x).equals(" ")) {
                 board.setMark(y, x, mark);
+                moves.add(new Move(x, y, mark));
                 return true;
             }
         }
@@ -87,16 +98,28 @@ public class Computer implements Serializable {
 
     /**
      * Method for when computes makes a move
-     * First checks if it can win
-     * If not checks if player can win
-     * If not makes a random move
+     * Looks for InArow amount of squares in a row and tries to win or block the opponent
+     * If in a row = 1, makes a random move
+     * 
+     * @return true after made a move for testing purposes
      */
     public boolean makeMove() {
-        if (!checkWin()) {
-            if (!checkBlock()) {
-                randomMove();
+        int inARow = 4;
+        while (true) {
+            if (!checkWin(inARow)) {
+                if (!checkBlock(inARow)) {
+                    inARow--;
+                    if (inARow == 1) {
+                        randomMove();
+                        return true;
+                    }
+                } else {
+                    return true;
+                }
+            } else {
+                return true;
             }
         }
-        return true;
     }
 }
+ 
