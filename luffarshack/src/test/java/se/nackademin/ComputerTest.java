2030b22e8da58e89163f0f8af966f927acfcdede
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-26 21:05:12 +0100

    updated computertest

--- luffarshack/src/test/java/se/nackademin/ComputerTest.java
+++ luffarshack/src/test/java/se/nackademin/ComputerTest.java
@@ -1,9 +1,12 @@
 package se.nackademin;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +14,8 @@ public class ComputerTest {
 
     InfiniteBoard board = new InfiniteBoard(0);
     Computer computer = new Computer(board);
-
+    List<Move> moves = new ArrayList<Move>();
+    
     /**
      * Test to see if computer will place a winning move
      * when 3 "0" in a row
@@ -19,12 +23,13 @@ public class ComputerTest {
     @Test
     public void winCheckTest() {
 
+        computer.setMoves(moves);
         board.setMark(0, 0, "0");
         board.setMark(0, 1, "0");
         board.setMark(0, 2, "0");
 
-        computer.checkWin();
-        assertTrue(board.checkWin("0"));
+        computer.checkWin(4);
+        assertTrue(board.checkWin("0", 4));
         assertEquals(board.getMark(0, 3), "0");
     }
 
@@ -35,8 +40,8 @@ public class ComputerTest {
         board.setMark(0, 1, "0");
         board.setMark(0, 2, "X");
 
-        computer.checkWin();
-        assertFalse(board.checkWin("X"));
+        computer.checkWin(4);
+        assertFalse(board.checkWin("X", 4));
         assertEquals(board.getMark(0, 3), " ");
     }
 
@@ -47,11 +52,12 @@ public class ComputerTest {
     @Test
     public void checkBlockTest() {
 
+        computer.setMoves(moves);
         board.setMark(0, 0, "X");
         board.setMark(0, 1, "X");
         board.setMark(0, 2, "X");
 
-        computer.checkBlock();
+        computer.checkBlock(4);
         assertEquals(board.getMark(0, 3), "0");
     }
 
@@ -63,17 +69,19 @@ public class ComputerTest {
         board.setMark(0, 2, "0");
 
         
-        assertFalse(computer.checkBlock());
+        assertFalse(computer.checkBlock(4));
         assertEquals(board.getMark(0, 3), " ");
     }
 
     @Test
     public void randomMoveTest() {
+        computer.setMoves(moves);
         assertTrue(computer.randomMove());
     }
 
     @Test
     public void makeMoveTest() {
+        computer.setMoves(moves);
         assertTrue(computer.makeMove());
     }
 }
