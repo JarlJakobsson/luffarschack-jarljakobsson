fb63e4efbe22113451659106042d82ec4edaa795
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-26 20:55:22 +0100

    Added replay method and improved logic

--- luffarshack/src/main/java/se/nackademin/Game.java
+++ luffarshack/src/main/java/se/nackademin/Game.java
@@ -1,33 +1,58 @@
 package se.nackademin;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game implements Serializable {
     private Player player1;
     private Player player2;
     private InfiniteBoard board;
     private Stats stats;
-    private Game game;
     private boolean isComputerGame = false;
-    private String player1Name;
     private Computer computer;
     private boolean isPlayer1Turn = true;
     private PlacementMenu placementMenu;
+    private List<Move> moves;
 
+    /**
+     * Constructor for Two Player Game
+     * @param stats       takes in a stats class to update stats during the game
+     * @param player1Name takes in player1name to create player 1
+     * @param player2Name takes in player2name to create player 2
+     * @param boardSize   takes in boardsize to create correct board
+     */
     public Game(Stats stats, String player1Name, String player2Name, int boardSize) {
         this.stats = stats;
         board = new InfiniteBoard(boardSize);
         player1 = new Player(player1Name, "X");
         player2 = new Player(player2Name, "0");
         placementMenu = new PlacementMenu(board);
+        moves = new ArrayList<Move>();
     }
-    
+
+    /**
+     * Constructor for Game against Computer
+     * @param stats
+     * @param player1Name
+     * @param boardSize
+     */
     public Game(Stats stats, String player1Name, int boardSize) {
         this.stats = stats;
         isComputerGame = true;
         board = new InfiniteBoard(boardSize);
-        computer = new Computer(board);
         player1 = new Player(player1Name, "X");
+        placementMenu = new PlacementMenu(board);
+        moves = new ArrayList<Move>();
+        computer = new Computer(board);
+    }
+
+    public void setPlacementMenu(PlacementMenu placementMenu) {
+        this.placementMenu = placementMenu;
+    }
+
+    public void appendMove(int row, int col, String playerMark) {
+        moves.add(new Move(row, col, playerMark));
     }
 
     public Stats getStats() {
@@ -50,14 +75,6 @@ public class Game implements Serializable {
         return isComputerGame;
     }
 
-    public Game getGame() {
-        return game;
-    }
-
-    public void setGame(Game game) {
-        this.game = game;
-    }
-
     public Player getPlayer() {
         return player1;
     }
@@ -74,57 +91,60 @@ public class Game implements Serializable {
         this.player2 = player2;
     }
 
-    public void initializeGame() {
-        // set the cursor position to the last move made by the computer
-        board.setCursorX(0);
-        board.setCursorY(0);
-        // update any other necessary state variables here
-    }
-
     /**
-     * Method to start a two player game.
-     * Creates a new board, players and a placement menu.
-     * Method handles the game loop by alternating between player 1 and player 2
-     * until a winner or quitter
+     * Method to handle the twoPlayerGameLoop
+     * Keeps going until a winner or quitter
+     * before player 1 moves checks if its player1s turn
+     * incase of loading a saved game during player2s turn
      * 
-     * TODO make none hardcoded markers.
-     * 
-     * @param stats       takes in a stats class to update stats during the game
-     * @param player1Name takes in player1name to create player 1
-     * @param player2Name takes in player2name to create player 2
-     * @param boardSize   takes in boardsize to create correct board
-     * 
+     * Changes playerturn variable after each move unless quitter
      */
     public boolean twoPlayerGame() {
-        while (!placementMenu.getQuit() && !board.checkWin(player2.getMark())) {
-            if(isPlayer1Turn){
-                placementMenu.runMenu(player1, stats, game);
+        while (!placementMenu.getQuit() && !placementMenu.getWinner()) {
+            if(isPlayer1Turn) {
+                placementMenu.runMenu(player1, stats, this);
                 isPlayer1Turn = false;
             }
             if (!placementMenu.getQuit() && !placementMenu.getWinner()) {
-                placementMenu.runMenu(player2, stats, game);
+                placementMenu.runMenu(player2, stats, this);
                 if(!placementMenu.getQuit()) {
                     isPlayer1Turn = true;
                 }
             } else {
-                break;
+                return true;
             }
-        } return true;
+        } return false;
     }
 
+    /**
+     * Method to handle computerGameLoop
+     */
     public void computerGame() {
-        PlacementMenu placementMenu = new PlacementMenu(board);
-        Computer computer = new Computer(board);
+        computer.setMoves(moves);
         while (!placementMenu.getQuit() && !placementMenu.getWinner()) {
-            placementMenu.runMenu(player1, stats, game);
+            placementMenu.runMenu(player1, stats, this);
             if (!placementMenu.getQuit() && !placementMenu.getWinner()) {
                 stats.setTotalMoves(stats.getTotalMoves() + 1);
                 computer.makeMove();
-                if (board.checkWin(computer.getMark())) {
+                if (board.checkWin(computer.getMark(), 4)) {
                     stats.setFinnishedGames(stats.getFinnishedGames() + 1);
                     placementMenu.setWinner(true);
                     System.out.println("Computer: EZ  WIN");
                 }
+            }
+        }
+    }
+
+    public void replay() {
+        board.clearBoard();
+        for (Move move : moves) {
+            board.setMark(move.row, move.col, move.playerMark);
+            board.printBoard();
+            System.out.println("Player " + move.playerMark + " places mark on row " + move.row + ", col " + move.col);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
